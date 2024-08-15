851cba72722d328c1f2c32842d9962429c2c48f1
pabitra <pabitrapandit2001@gmail.com>
2024-08-15 20:51:38 +0530

    Review Version 4 Chat Update

---
+++ assets/js/loadBotPenguin.js
@@ -0,0 +1,8 @@
+(function() {
+    var script = document.createElement('script');
+    script.src = 'https://cdn.botpenguin.com/website-bot.js';
+    script.defer = true;
+    script.textContent = '66b4f6e62f7fbe307458098c,66b4f59e7df117d9fe3bd654'; // Optional: If needed to pass data
+    document.head.appendChild(script);
+  })();
+  
